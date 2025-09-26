90352a341b6d99917a9de2b8d22547a3635a466d
Emmymoks <mokwunyeemmanuel@gmail.com>
2025-09-26 14:42:05 +0100

    Update tailwind.config.cjs

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,13 +1,17 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {
       colors: {
-        warmbg: '#FFF7F2',
-        softpink: '#FFBBB7',
-        coral: '#FF6B6B'
+        warmbg: "#FFF7F2",
+        softpink: "#FFBBB7",
+        coral: "#FF6B6B"
       }
     }
   },
-  plugins: [],
+  plugins: []
 }
